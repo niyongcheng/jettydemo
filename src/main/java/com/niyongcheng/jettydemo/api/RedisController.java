484af853ed0ceeda20f473bcc7f8e2bfa694df01
sid.ni <sid.ni@ef.com>
2020-04-20 15:49:36 +0800

    #add

--- src/main/java/com/niyongcheng/jettydemo/api/RedisController.java
+++ src/main/java/com/niyongcheng/jettydemo/api/RedisController.java
@@ -9,8 +9,7 @@ import java.nio.file.Path;
 @RestController
 public class RedisController {
     //
-    @RequestMapping(value = "/",
-                    method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String ReturnDefaultPage() {
         return "Hello Spring Boot";
     }
